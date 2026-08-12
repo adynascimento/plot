ea2c618fe6e18b6a7d5334e430639fd2b2cd9889
adynascimento <adyllyson@outlook.com>
2026-08-12 00:09:05 -0300

    add right-side y-axis to vertical colorbar

--- plotter/colorbar.go
+++ plotter/colorbar.go
@@ -27,6 +27,7 @@ func (plt *plotParameters) drawVerticalColorbar(xwidth, ywidth font.Length) *vgi
 	// create a new plot for vertical colorbar
 	p := plot.New()
 	p.HideX()
+	p.HideY()
 	p.Y.Padding = 0
 	c := &plotter.ColorBar{
 		ColorMap: &colormapPalette{
@@ -41,7 +42,7 @@ func (plt *plotParameters) drawVerticalColorbar(xwidth, ywidth font.Length) *vgi
 	// spacing between plot and colorbar
 	spacing := font.Length(1.5) * vg.Centimeter
 
-	img := vgimg.New(xwidth+1.35*spacing, ywidth)
+	img := vgimg.New(xwidth+1.5*spacing, ywidth)
 	dCanvas := draw.New(img)
 
 	// draw the principal plot in the first column
@@ -57,10 +58,35 @@ func (plt *plotParameters) drawVerticalColorbar(xwidth, ywidth font.Length) *vgi
 	p.Draw(draw.Canvas{
 		Canvas: dCanvas,
 		Rectangle: vg.Rectangle{
-			Min: vg.Point{X: (xwidth + 0.3*spacing), Y: vg.Centimeter},
-			Max: vg.Point{X: (xwidth + 1.3*spacing), Y: 0.93 * ywidth},
+			Min: vg.Point{X: (xwidth + 0.3*spacing), Y: 1.25 * vg.Centimeter},
+			Max: vg.Point{X: (xwidth + 0.7*spacing), Y: ywidth - 0.65*vg.Centimeter},
 		},
 	})
+
+	// create a separate plot for the right y-axis
+	axisPlot := plot.New()
+	axisPlot.HideX()
+	axisPlot.Y.Min = plt.colorbar.min
+	axisPlot.Y.Max = plt.colorbar.max
+	axisPlot.Y.Padding = 0
+
+	// draw the y-axis on the right side of the colorbar
+	drawRightYAxis(
+		draw.Canvas{
+			Canvas: dCanvas,
+			Rectangle: vg.Rectangle{
+				Min: vg.Point{
+					X: xwidth + 0.7*spacing,
+					Y: 1.44 * vg.Centimeter,
+				},
+				Max: vg.Point{
+					X: xwidth + 1.0*spacing,
+					Y: ywidth - 0.67*vg.Centimeter,
+				},
+			},
+		},
+		axisPlot.Y,
+	)
 
 	return img
 }
@@ -82,7 +108,7 @@ func (plt *plotParameters) drawHorizontalColorbar(xwidth, ywidth font.Length) *v
 	// spacing between plot and colorbar
 	spacing := font.Length(1.5) * vg.Centimeter
 
-	img := vgimg.New(xwidth, ywidth+spacing)
+	img := vgimg.New(1.05*xwidth, ywidth+spacing)
 	dCanvas := draw.New(img)
 
 	// draw the principal plot in the first row
@@ -98,10 +124,65 @@ func (plt *plotParameters) drawHorizontalColorbar(xwidth, ywidth font.Length) *v
 	p.Draw(draw.Canvas{
 		Canvas: dCanvas,
 		Rectangle: vg.Rectangle{
-			Min: vg.Point{X: vg.Centimeter, Y: 0},
-			Max: vg.Point{X: 0.99 * xwidth, Y: 1.2 * vg.Centimeter},
+			Min: vg.Point{X: 1.22 * vg.Centimeter, Y: 0},
+			Max: vg.Point{X: xwidth, Y: 1.2 * vg.Centimeter},
 		},
 	})
 
 	return img
+}
+
+func drawRightYAxis(c draw.Canvas, a plot.Axis) {
+	marks := a.Tick.Marker.Ticks(a.Min, a.Max)
+
+	// draw axis line
+	x := c.Min.X
+	c.StrokeLine2(a.LineStyle, x, c.Min.Y, x, c.Max.Y)
+
+	// draw ticks
+	major := false
+	length := a.Tick.Length
+	if a.Tick.Width > 0 && a.Tick.Length > 0 {
+		for _, t := range marks {
+			y := c.Y(a.Norm(t.Value))
+			if !c.ContainsY(y) {
+				continue
+			}
+			end := length
+			if t.IsMinor() {
+				end = length / 2
+			}
+			c.StrokeLine2(a.Tick.LineStyle, x, y, x+end, y)
+			if !t.IsMinor() {
+				major = true
+			}
+		}
+	}
+
+	// leave the same spacing used by Gonum between ticks and labels
+	labelX := x + length
+	if major {
+		labelX += a.Tick.Label.Width(" ")
+	}
+
+	// draw labels to the right of the ticks
+	labelStyle := a.Tick.Label
+	labelStyle.XAlign = draw.XLeft
+	for _, t := range marks {
+		if t.IsMinor() {
+			continue
+		}
+		y := c.Y(a.Norm(t.Value))
+		if !c.ContainsY(y) {
+			continue
+		}
+		c.FillText(
+			labelStyle,
+			vg.Point{
+				X: labelX,
+				Y: y + a.Tick.Label.FontExtents().Descent,
+			},
+			t.Label,
+		)
+	}
 }
